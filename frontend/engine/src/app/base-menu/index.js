8fd7a9ac1a46c627bcfce799068c766f9bcd2322
icluding <vanya_gusev99@mail.ru>
2019-08-12 16:59:45 +0400

    dev

--- frontend/engine/src/app/base-menu/index.js
+++ frontend/engine/src/app/base-menu/index.js
@@ -9,7 +9,42 @@ export default class BaseMenu extends  React.Component {
 
     render(){
         return(
-            <div className="test">123</div>
+                <div className="menu">
+                    <div className="bg-logo">
+                        <div className="logo"></div>
+                    </div>
+                    <div className="wrapper-modes">
+                        <div className="modes-control">
+                            <button type='submit'></button>
+                        </div>
+                        <div className="modes">
+                            <a href="#"><span className="usually"></span>Обычная игра(N x N)</a>
+                            <a href="#"><span className="duel"></span>Дуэль(1 х 1)</a>
+                            <a href="#"><span className="regulation"></span>Правила и режимы</a>
+                            <a href="#"><span className="options"></span>Настроить змейку</a>
+                            <a href="#"><span className="report"></span>Жалобы и предложения</a>
+                        </div>
+                    </div>
+                    <div className="chat">
+                        <div className="wrapper-chat">
+                            <div className="list-chat"></div>
+                            <div className="communication">
+                                <div></div>
+                                <input type="text"></input>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
 
         )
     }
